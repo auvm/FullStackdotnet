8cd392ed1b7a4a9977d559729edf18f8e6f52cdd
auvm <angelurielvelascom@gmail.com>
2025-11-11 00:36:06 -0600

    añado primer clase POO

---
+++ POO/Program.cs
@@ -0,0 +1,28 @@
+﻿Beer erdinger = new Beer
+{
+    Name = "Erdinger",
+    Price = 3.33m
+};
+
+
+System.Console.WriteLine(erdinger.Name);
+var Corona = new Beer();
+Corona.Name = "Corona";
+Corona.Price = 2.99m;
+
+erdinger.getInfo();
+Corona.getInfo();
+
+
+public class Beer
+{
+    public string Name { get; set; }
+    public decimal Price { get; set; }
+
+    public void getInfo()
+    {
+        System.Console.WriteLine($"Nombre: {Name} Precio: ${Price}");
+    }
+
+
+}
